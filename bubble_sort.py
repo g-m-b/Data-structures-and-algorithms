c13fc1ac81b385666a0b65446a2d90d140a441e3
Mahesh Babu Gurram <64735929+g-m-b@users.noreply.github.com>
2020-06-29 13:00:04 +0530

    Added redability

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,12 @@
-def bubble_sort(a):
-    n=len(a)
-    for i in range(n):
-        for j in range(0,n-i-1):
-            if a[j]>a[j+1]:
-                a[j],a[j+1]=a[j+1],a[j]
+'''
+The def takes the array, the elements are swapped in place.
+'''
+def bubble_sort(arr):
+    arr_len=len(arr)
+    for i in range(arr_len):
+        for j in range(0,arr_len-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
 if __name__ == '__main__':
     a=[1,5,3,2,7,9,10]
     bubble_sort(a)
